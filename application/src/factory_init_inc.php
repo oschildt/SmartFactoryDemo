d2f374ddea67a6e2079b95aa3fe87e8ec7ae8368
oschildt <oleg.schildt@gmail.com>
2018-12-12 16:33:23 +0300

    Api handler fix for flexible stopping the handling

--- application/src/factory_init_inc.php
+++ application/src/factory_init_inc.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace MyApplication;
 
 use SmartFactory\Interfaces\ISessionManager;
@@ -26,59 +27,64 @@ use MyApplication\MySessionManager;
 // Overriding the default binding
 FactoryBuilder::bindClass(ISessionManager::class, MySessionManager::class);
 //-------------------------------------------------------------------
-FactoryBuilder::bindClass(IDebugProfiler::class, DebugProfiler::class, function($instance) {
-  $instance->init(["log_path" => approot() . "logs/"]);
+FactoryBuilder::bindClass(IDebugProfiler::class, DebugProfiler::class, function ($instance) {
+    $instance->init(["log_path" => approot() . "logs/"]);
 });
 //-------------------------------------------------------------------
-FactoryBuilder::bindClass(IErrorHandler::class, ErrorHandler::class, function($instance) {
-  $instance->init(["app_root" => approot(), "log_path" => approot() . "logs/"]);
+FactoryBuilder::bindClass(IErrorHandler::class, ErrorHandler::class, function ($instance) {
+    $instance->init(["app_root" => approot(), "log_path" => approot() . "logs/"]);
 });
 //-------------------------------------------------------------------
-FactoryBuilder::bindClass(ILanguageManager::class, LanguageManager::class, function($instance) {
-  $instance->init(["localization_path" => approot() . "localization/"]);
-  $instance->loadDictionary();
-  $instance->detectLanguage();
+FactoryBuilder::bindClass(ILanguageManager::class, LanguageManager::class, function ($instance) {
+    $instance->init(["localization_path" => approot() . "localization/"]);
+    $instance->loadDictionary();
+    $instance->detectLanguage();
 });
 //-------------------------------------------------------------------
-FactoryBuilder::bindClass(ConfigSettingsManager::class, ConfigSettingsManager::class, function($instance) {
-  $instance->init(["save_path" => approot() . "../config/settings.xml",
-                   "config_file_must_exist" => false
-                   //"save_encrypted" => true,
-                   //"salt_key" => "demotest"
-                  ]);
-  $instance->loadSettings();
-
-  $instance->setValidator(new ConfigSettingsValidator());
+FactoryBuilder::bindClass(ConfigSettingsManager::class, ConfigSettingsManager::class, function ($instance) {
+    $instance->init([
+        "save_path" => approot() . "../config/settings.xml",
+        "config_file_must_exist" => false
+        //"save_encrypted" => true,
+        //"salt_key" => "demotest"
+    ]);
+    $instance->loadSettings();
+    
+    $instance->setValidator(new ConfigSettingsValidator());
 });
 //-------------------------------------------------------------------
-FactoryBuilder::bindClass(ApplicationSettingsManager::class, ApplicationSettingsManager::class, function($instance) {
-  $instance->init(["dbworker" => dbworker(),
-                   "settings_table" => "SETTINGS",
-                   "settings_column" => "DATA"
-                  ]);
-  $instance->loadSettings();
-
-  $instance->setValidator(new ApplicationSettingsValidator());
+FactoryBuilder::bindClass(ApplicationSettingsManager::class, ApplicationSettingsManager::class, function ($instance) {
+    $instance->init([
+        "dbworker" => dbworker(),
+        "settings_table" => "SETTINGS",
+        "settings_column" => "DATA"
+    ]);
+    $instance->loadSettings();
+    
+    $instance->setValidator(new ApplicationSettingsValidator());
 });
 //-------------------------------------------------------------------
-FactoryBuilder::bindClass(UserSettingsManager::class, UserSettingsManager::class, function($instance) {
-  $instance->init(["dbworker" => dbworker(),
-                   "user_table" => "USERS",
-                   "settings_fields" => [
-                      "ID" => DBWorker::DB_NUMBER,
-                      "SIGNATURE" => DBWorker::DB_STRING,
-                      "STATUS" => DBWorker::DB_STRING,
-                      "HIDE_PICTURES" => DBWorker::DB_NUMBER,
-                      "HIDE_SIGNATURES" => DBWorker::DB_NUMBER,
-                      "LANGUAGE" => DBWorker::DB_STRING,
-                      "TIME_ZONE" => DBWorker::DB_STRING
-                   ],
-                   "user_id_field" => "ID",
-                   "user_id_getter" => function() { return 1; }
-                  ]);
-  $instance->loadSettings();
-
-  $instance->setValidator(new UserSettingsValidator());
+FactoryBuilder::bindClass(UserSettingsManager::class, UserSettingsManager::class, function ($instance) {
+    $instance->init([
+        "dbworker" => dbworker(),
+        "user_table" => "USERS",
+        "settings_fields" => [
+            "ID" => DBWorker::DB_NUMBER,
+            "SIGNATURE" => DBWorker::DB_STRING,
+            "STATUS" => DBWorker::DB_STRING,
+            "HIDE_PICTURES" => DBWorker::DB_NUMBER,
+            "HIDE_SIGNATURES" => DBWorker::DB_NUMBER,
+            "LANGUAGE" => DBWorker::DB_STRING,
+            "TIME_ZONE" => DBWorker::DB_STRING
+        ],
+        "user_id_field" => "ID",
+        "user_id_getter" => function () {
+            return 1;
+        }
+    ]);
+    $instance->loadSettings();
+    
+    $instance->setValidator(new UserSettingsValidator());
 });
 
 //-------------------------------------------------------------------
